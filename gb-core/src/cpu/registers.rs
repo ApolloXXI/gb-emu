abb113019b427043628c4268babb61632928d42b
Emmanuel Mbuse <emmanuelmbuse@gmail.com>
2025-08-18 00:13:00 +0200

    Simple bit manipulation

--- gb-core/src/cpu/registers.rs
+++ gb-core/src/cpu/registers.rs
@@ -9,3 +9,15 @@ pub struct Registers {
     pub h: u8,
     pub l: u8,
 }
+
+impl Registers {
+    pub fn get_bc(&self) -> u16 {
+        (self.b as u16) << 8
+        | self.c as u16
+    }
+
+    pub fn set_bc(&mut self, value: u16){
+        self.b = ((value & 0xFF00) >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
+    }
+}
